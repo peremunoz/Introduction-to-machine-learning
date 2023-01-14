196e9448088b3b4d943844901f5751bdd6a69d98
peremunoz <73690199+peremunoz@users.noreply.github.com>
2023-01-14 20:05:33 +0100

    Implemented all.

--- clusters.py
+++ clusters.py
@@ -1,3 +1,4 @@
+import random
 from typing import Tuple, List
 from math import sqrt
 
@@ -21,7 +22,10 @@ def readfile(filename: str) -> Tuple[List, List, List]:
 # .........DISTANCES........
 # They are normalized between 0 and 1, where 1 means two vectors are identical
 def euclidean(v1, v2):
-    distance = 0  # TODO
+    distance = 0
+    for i in range(len(v1)):
+        distance += (v1[i] - v2[i])**2
+    distance = sqrt(distance)
     return 1 / (1+distance)
 
 def euclidean_squared(v1, v2):
@@ -107,7 +111,85 @@ def printclust(clust: BiCluster, labels=None, n=0):
 
 
 # ......... K-MEANS ..........
-def kcluster(rows, distance, k=4):
-    # TODO
-    raise NotImplementedError
+def kcluster(rows, distance=euclidean_squared, k=4, num_executions=10):
 
+    best_config = (None, float('inf'))
+
+    for i in range(num_executions):
+
+        # Determine the minimum and maximum values for each point
+        ranges = [(min([row[i] for row in rows]),
+        max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+        # Create k randomly placed centroids
+        clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+
+        lastmatches = None
+        sum_distances = 0
+        for t in range(100):
+            bestmatches = [[] for _ in range(k)]
+            bestdistances = [0 for _ in range(len(rows))]
+
+            # Find which centroid is the closest for each row
+            for j in range(len(rows)):
+                row = rows[j]
+                bestmatch = 0
+                bestdistance = distance(row, clusters[0])
+
+                for i in range(k):
+                    d = distance(clusters[i], row)
+                    if d < distance(clusters[bestmatch], row):
+                        bestmatch = i
+                        bestdistance = d
+
+                bestmatches[bestmatch].append(j)
+                bestdistances[j] = bestdistance
+
+            # If the results are the same as last time, done
+            if bestmatches == lastmatches: break
+            lastmatches = bestmatches
+
+            # Move the centroids to the average of their members
+            for i in range(k):
+                avgs = [0.0] * len(rows[0])
+                if len(bestmatches[i]) > 0:
+                    for rowid in bestmatches[i]:
+                        for m in range(len(rows[rowid])):
+                            avgs[m] += rows[rowid][m]
+                    for j in range(len(avgs)):
+                        avgs[j] /= len(bestmatches[i])
+                    clusters[i] = avgs
+
+        if sum_distances < best_config[1]:
+            best_config = (clusters, sum(bestdistances))
+
+    return best_config
+
+
+def main():
+    blognames, words, data = readfile('blogdata_full.txt')
+
+    print("K-Means clustering")
+
+    kclust = kcluster(data, num_executions=1)
+
+    print("Clusters:")
+    for cluster in kclust[0]:
+        print(cluster)
+
+
+    print("Sum of distances:", kclust[1])
+    print()
+
+    print("Clustering evaluation:")
+
+    print("Total sum of distances for different k values.")
+    for k in range(1, 11):
+        kclust = kcluster(data, k=k, num_executions=3)
+        print(f"k={k}:", kclust[1])
+    print()
+
+
+
+if __name__ == "__main__":
+    main()
